fe2419e4f9e0f09bdc022b9128191199ec7a8ab4
Isabel Eclipse <Tardes@GET-09>
2025-09-11 20:38:39 +0200

    primer commit Usuario

--- src/cineBambu/Usuario.java
+++ src/cineBambu/Usuario.java
@@ -1,5 +1,18 @@
 package cineBambu;
 
+//nombre y/o DNI/ID. (Puedes añadir edad/saldo si lo ves útil, pero no es obligatorio.)
 public class Usuario {
 	//Isabel
+	private String nombre;
+	private String dni;
+	
+	//constructor:
+	public Usuario(String nombre, String dni) {
+		// TODO Auto-generated constructor stub
+
+		this.nombre = nombre;
+		this.dni = dni;
+
+	}
+
 }
