6103c53717b6b884d5a45a5012a29b3fe476bd84
Isabel Eclipse <Tardes@GET-09>
2025-09-11 20:35:33 +0200

    primer commit cine

--- src/cineBambu/Cine.java
+++ src/cineBambu/Cine.java
@@ -1,6 +1,20 @@
 package cineBambu;
 
+import java.util.Scanner;
+
+//nombre, salas (array de SalaDeCine).
 public class Cine {
 
 	//Isabel
+	
+	private String nombre;
+	private SalaDeCine[] salas=new SalaDeCine [5];
+	
+	//constructor
+	public Cine(String nombre, SalaDeCine[] salas)
+	{
+		
+	}
+	
+	
 }
