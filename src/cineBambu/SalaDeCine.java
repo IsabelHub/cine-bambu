c9ee8fdba65993fa2db16cbe4d0bb40309f8b3eb
AnaArroyo <Tardes@GET-01>
2025-09-11 20:44:28 +0200

    asientosLibres

--- src/cineBambu/SalaDeCine.java
+++ src/cineBambu/SalaDeCine.java
@@ -5,17 +5,17 @@ public class SalaDeCine {
 	//código/nombre, capacidad (entera), pelicula proyectada, butacas (array de Usuario del tamaño de la capacidad; null indica asiento libre).
 	
 	private int numSala;
-	private int capacidad;
 	private Pelicula pelicula;
-	private int[][] butacas;
+	private Usuario[][] butacas;
+	private int capacidad;
 	
 	//Genero un constructor con todos los atributos para que las peliculas no se generen con un campo por defecto
-	public SalaDeCine(int numSala, int capacidad, Pelicula pelicula, int[][] butacas) {
+	public SalaDeCine(int numSala, Pelicula pelicula, Usuario[][] butacas) {
 		
 		this.numSala = numSala;
-		this.capacidad = capacidad;
 		this.pelicula = pelicula;
 		this.butacas = butacas;
+		this.capacidad = butacas.length*butacas[0].length;
 	}
 	//No creamos set para que no se puedan modificar los atributos de la pelicula
 	public int getNumSala() {
@@ -27,8 +27,29 @@ public class SalaDeCine {
 	public Pelicula getPelicula() {
 		return pelicula;
 	}
-	public int[][] getButacas() {
+	public Usuario[][] getButacas() {
 		return butacas;
+	}
+	
+	public void asientosLibres() {
+		int contador = 0;
+		for (int i = 0; i < butacas.length; i++) { // Recorre filas
+			
+		    for (int j = 0; j < butacas[i].length; j++) { // Recorre columna
+		    	if(j==0) {
+		    		System.out.print(i);
+		    	}
+		        if (butacas[i][j]==null) {
+		        	contador++;
+		        	System.out.print("X ");
+		        } else {
+		        	System.out.println(j);
+		        }
+		    }
+		    System.out.println("Dispones de "+contador+" asientos libres");
+		}
+
+		
 	}
 	
 	
