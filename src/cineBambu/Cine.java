76693e796fda33257aba87594a0de2097e50c5b5
Isabel Eclipse <Tardes@GET-09>
2025-09-12 19:43:13 +0200

    cine

--- src/cineBambu/Cine.java
+++ src/cineBambu/Cine.java
@@ -12,7 +12,7 @@ public class Cine {
 	public Cine(String nombre, SalaDeCine[] salas)
 	{
 		this.nombre=nombre;
-		this.salas=new SalaDeCine [5];
+		this.salas=new SalaDeCine [3];
 	}
 
 	public String getNombre() {
@@ -27,36 +27,58 @@ public class Cine {
 	
 	  
 	// 1. Listar películas y disponibilidad
-	public void listarSalas() {
-		System.out.println("---- Listado de Salas ----");
-		for (SalaDeCine sala : salas) {
-			//recoger en una variable los asientos libres 
-			int libres = sala.asientosLibres();
-			
-			//calculo las plazas ocupadas  
-			int ocupados = sala.getCapacidad() - libres;
-
-			System.out.println("Sala #" + sala.getNumSala());
-			System.out.println("Película: " + sala.getPelicula().getTitulo());
-			System.out.println("Asientos ocupados: " + ocupados);
-			System.out.println("Asientos libres: " + libres);
-			System.out.println("------------------------------");
+	public void  listarSalas() {	
+			System.out.println("---- Listado de Salas ----");
+			for (SalaDeCine sala : salas) {
+				if(sala == null)
+				{
+					continue;
+				}
+				if (sala != null) {
+			        System.out.println("Sala #" + sala.getNumSala());
+			        System.out.println(sala.getPelicula().getId() + " Película: " + sala.getPelicula().getTitulo());
+			        System.out.println("------------------------------");
+			    }		
 		}
 	}
-	
+
+	// 2. Listar películas y disponibilidad
+	public void elegirPelicula(Usuario usuario) {
+		System.out.println("¿Que pelicula deseas ver?Inserta numero de la pelicula elegida: ");
+		Scanner sc = new Scanner(System.in);
+		int numPelicula = sc.nextInt();
+		for (int i = 0; i < salas.length; i++) {
+			if (salas[i] != null && numPelicula == salas[i].getPelicula().getId()) {
+				System.out.println("Has elegido la pelicula numero " + salas[i].getPelicula().getId() + "con titulo "
+						+ salas[i].getPelicula().getTitulo());
+				if (salas[i].asientosLibres() > 0) {
+					salas[i].asignarButacas(usuario);
+				}
+
+			}
+
+		}
+
+	}
+
 	/*
-	// 2. Obtener salas con al menos asientos libres 
-	public SalaDeCine[] getSalasConEspacio(int cantidad) {
-	//cantidad es el espacio total de una sala
+	// 2. Obtener salas con asientos libres 
+	public SalaDeCine[] SalasConEspacio(int cantidad) {
+		//cantidad es el espacio total de una sala
+		//salas es un array de tipo Sala de cine
 		// Primero, contar cuántas salas cumplen la condición
 		int contador = 0;
 		for (int i = 0; i < salas.length; i++) {
-			if (salas[i].asientosLibres     (cantidad)) {
+			
+			int numeroAsientos = salas[i].asientosLibres();
+			if(salas[i].hayEspacio())
+			{
 				contador++;
 			}
+			
 		}
 
-		// Crear un array con el tamaño justo
+		// Crear un array de salas disponibles con el tamaño justo de asientos
 		SalaDeCine[] disponibles = new SalaDeCine[contador];
 		int indice = 0;
 		for (int i = 0; i < salas.length; i++) {
@@ -69,18 +91,18 @@ public class Cine {
 		return disponibles;
 	}
 	
+	*/
 	
-
 	// 3. Mostrar ocupación por sala
 	public void mostrarOcupacionSalas() {
 		System.out.println("---- Mapa de ocupación por sala ----");
 		for (int i = 0; i < salas.length; i++) {
-			salas[i].mostrarMapaButacas();
+			salas[i].asientosLibres();
 			System.out.println();
 		}
 	}
 
-	 */
+	 
 	// 4. Obtener una sala por número (útil para seleccionar sala)
 	public SalaDeCine numeroSala(int numSala) {
 		for (int i = 0; i < salas.length; i++) {
