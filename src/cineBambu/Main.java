8b8266392e448d71900e1cf15b234ef1e01e003a
Isabel Eclipse <Tardes@GET-09>
2025-09-12 19:49:19 +0200

    actualizacion instancia salaCine

--- src/cineBambu/Main.java
+++ src/cineBambu/Main.java
@@ -36,8 +36,8 @@ public class Main {
 		Usuario[][] butacas = new Usuario[8][10];
 		
 		SalaDeCine sala1 = new SalaDeCine(1,pelicula1 , butacas);
-		SalaDeCine sala2 = new SalaDeCine(1,pelicula2 , butacas);
-		SalaDeCine sala3 = new SalaDeCine(1,pelicula3 , butacas);
+		SalaDeCine sala2 = new SalaDeCine(2,pelicula2 , butacas);
+		SalaDeCine sala3 = new SalaDeCine(3,pelicula3 , butacas);
 		
 		SalaDeCine[] salas = {sala1, sala2, sala3};
 		
